82b8c810599f69536f2612327a5f7358422ea508
hite5h <66128497+hite5h@users.noreply.github.com>
2020-10-02 00:22:17 +0530

    Update REV NO.cpp

--- REV NO.cpp
+++ REV NO.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
  
 long long int reverse(long long int n){
-    long long int rem=0;
-    while(n != 0){
+    long long int rem=0;	//rem stands for reminder
+    while(n != 0){		//as long as number is not equal to 0 we traverse
         rem = rem * 10;
         rem= rem + n%10;
         n= n/10;
     }
-	return rem;
+	return rem;		//returning reminder
 }
 int main(void)
 {
@@ -15,11 +15,11 @@ int main(void)
     scanf("%d",&n);
  
     while(n--){
-    	long long int a,b;
+    	long long int a,b;		//a=number1 //b=number2
     	scanf("%lld%lld",&a,&b);
-    	a =reverse(a);
-    	b =reverse(b);
-    	long long int sum_rev =reverse(a+b);
+    	a =reverse(a);			//a number pass to function
+    	b =reverse(b);			//a number pass to function
+    	long long int sum_rev =reverse(a+b); //calling reverse function
     	printf("%lld\n",sum_rev);
     }
     return 0;
